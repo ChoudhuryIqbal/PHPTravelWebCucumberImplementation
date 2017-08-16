79b249eca3ef5e08f2effd9c2c6fa7fbcf610d13
Iqbal <iqbalchoudhury2@gmail.com>
2017-08-16 15:51:13 -0500

    homepage updated

---
+++ src/main/java/com/phptravel/cucumber/PhpTravelCucumber/uiAction/HomePage.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.phptravel.cucumber.PhpTravelCucumber.uiAction;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.phptravel.cucumber.PhpTravelCucumber.testBase.TestBase;
+
+
+
+/**
+ * @author choudhuryIqbal
+ *
+ */
+public class HomePage extends TestBase {
+	
+		public static final Logger log = Logger.getLogger(HomePage.class.getName());
+		WebDriver driver;
+
+		@FindBy(linkText="My Account")
+		private WebElement accountLocator;
+		
+		@FindBy(linkText="Sign Up")
+		private WebElement signUpButtonLocator;
+
+		
+		public HomePage(WebDriver driver) {
+			this.driver=driver;
+			PageFactory.initElements(driver,this);
+		}
+
+
+		
+		public void clickOnMyAccount() {
+					accountLocator.click();
+					log.info("==========My account is clicked===========");
+					signUpButtonLocator.click();
+					log.info("==========Sign up button is clicked=========");
+		}
+
+
+		
+		public void clickOnSignUp() {
+		
+			
+		}
+		
+		
+	
+
+
+}
